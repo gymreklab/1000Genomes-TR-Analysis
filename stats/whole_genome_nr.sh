0c85cb47e3985355b2193a0f2a2c397e94341fcc
Helyaneh Ziaei-jam <hziaeija@tscc-login2.sdsc.edu>
2022-08-02 19:27:36 -0700

    extract info for gt across wg

---
+++ stats/whole_genome_nr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#PBS -N sample_nr
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=02:00:00
+#PBS -o /projects/ps-gymreklab/helia/ensembl/experiments/stats/log
+#PBS -e /projects/ps-gymreklab/helia/ensembl/experiments/stats/log
+#PBS -A gymreklab-group
+#PBS -q hotel
+#PBS -V
+
+# Getting GT for each sample
+cd /projects/ps-gymreklab/helia/ensembl/experiments/stats
+
+bcftools query -f '[%CHROM\t%POS\t%SAMPLE\t%GT\n]' -i 'GT="alt"' /projects/ps-gymreklab/helia/ensembl/ensemble_out/merged_chr"$chr"_sorted_ver2.vcf.gz > info/gt_chr"$chr".txt
+
