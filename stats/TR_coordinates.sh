204e210b8195af1576c4bff7161325c202a8b067
Helyaneh Ziaei-jam <hziaeija@tscc-login2.sdsc.edu>
2022-09-14 12:11:27 -0700

    adding code for allele frequencies and update TR coordinates

--- stats/TR_coordinates.sh
+++ stats/TR_coordinates.sh
@@ -1,10 +1,12 @@
-
-# Get TR coordinates
-rm /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/all_TR_coordinates.bed
-for i in {1..22}
-do
-	bcftools query  -f '%CHROM\t%POS\t%END\n' /projects/ps-gymreklab/helia/ensembl/ensemble_out/merged_chr"$i"_sorted_ver2.vcf.gz >> /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/all_TR_coordinates.bed
-done
-
-# Intersect with genes
-bedtools intersect -a /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/all_TR_coordinates.bed -b /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/coding_regions_sorted_corrected.bed -u > /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/intersect.txt
+#
+### Get TR coordinates
+#rm /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/all_TR_coordinates.bed
+#for i in {1..22}
+#do
+#	bcftools query  -f '%CHROM\t%POS\t%END\n' /projects/ps-gymreklab/helia/ensembl/ensemble_out/merged_chr"$i"_sorted_ver2.vcf.gz >> /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/all_TR_coordinates.bed
+#done
+#
+## get mapping
+#bedtools intersect -a /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/all_TR_coordinates.bed -b /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/coding_regions_sorted_corrected.bed -wb -wa > /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/mapping.txt
+#
+awk '{ print $4, $5, $6; OFS="\t"  }' /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/mapping.txt > /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/overlapping_genes_coordinates.txt
