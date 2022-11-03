8cacbb6c7134ab1e06db84ca5d387a967945cc64
heliziii <hziaeija@ucsd.edu>
2022-11-03 13:54:13 -0700

    update diff_dist

--- diff_dist/preprocess_data.py
+++ diff_dist/preprocess_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 from tqdm import tqdm
 
 if __name__ == '__main__':
-	data_dir_path = '/projects/ps-gymreklab/helia/ensembl/experiments/charact/diff_dist/diff'
+	data_dir_path = '/gymreklab-tscc/helia/ensembl/experiments/charact/diff_dist/diff'
 
 	chroms = list(range(1, 23))
 	super_pops = ['AMR', 'AFR', 'EAS', 'EUR', 'SAS']
@@ -22,7 +22,8 @@ if __name__ == '__main__':
 			with open(os.path.join(data_dir_path, f'{pop}_{chrom}_diff.txt'), 'r') as f:
 				file_data = []
 
-				while (line := f.readline().rstrip()):
+				for line in f:
+					line = line.rstrip()
 					# First three positions of line are chr, pos, and motif len
 					line = line.split('\t')
 					chrom_str = line.pop(0)
