23827bb638138f7322ac200cac0de9ff34e1097c
Helyaneh Ziaei-jam <hziaeija@tscc-login2.sdsc.edu>
2022-12-04 18:52:51 -0800

    update files for variant per pop

--- stats/variant_runner.sh
+++ stats/variant_runner.sh
@@ -1,8 +1,9 @@
-subpops=('ACB' 'ASW' 'BEB' 'CDX' 'CEU' 'CHB' 'CHS' 'CLM' 'ESN' 'FIN' 'GBR' 'GIH' 'GWD' 'IBS' 'ITU' 'JPT' 'KHV' 'LWK' 'MSL' 'MXL' 'PEL' 'PJL' 'PUR' 'STU' 'TSI' 'YRI')
+subpops=('ACB' 'ASW' 'BEB' 'CDX' 'CEU' 'CHB' 'CHS' 'CLM' 'ESN' 'FIN' 'GBR' 'GIH' 'GWD' 'IBS' 'ITU' 'JPT' 'KHV' 'LWK' 'MSL' 'MXL' 'PEL' 'PJL' 'PUR' 'STU' 'TSI' 'YRI' 'Benin' 'Botswana' 'Burkina_Faso-Ghana' 'Cameroon' 'Mali' 'Nigeria' 'Zambia')
 for subpop in "${subpops[@]}"
 do
-  for chrom in {1..22}
+  for chrom in {12}
   do
+	chrom="12"
         echo $subpop $chrom
 	      qsub variant_pop.pbs -v pop_name="$subpop",chr=$chrom
 	done
