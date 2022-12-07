4b9fda5820c6539215e22a90f97320a868948df7
Helyaneh Ziaei-jam <hziaeija@tscc-login2.sdsc.edu>
2022-12-07 15:54:14 -0800

    update MI analysis scripts

--- mend/readme.sh
+++ mend/readme.sh
@@ -14,7 +14,7 @@ cd /projects/ps-gymreklab/helia/ensembl/1000Genomes-TR-Analysis/mend
 
 source /projects/ps-gymreklab/helia/ensembl/venv_3.9/bin/activate
 
-./MI_gt_analysis.py /projects/ps-gymreklab/helia/TR_1000G/1000G.ped chr${chrom} /projects/ps-gymreklab/helia/ensembl/ensemble_out/ensemble_chr${chrom}_dropped.vcf.gz > /projects/ps-gymreklab/helia/ensembl/1000Genomes-TR-Analysis/mend/result/mend_gt_chr${chrom}.tab
+./MI_gt_analysis.py /projects/ps-gymreklab/helia/TR_1000G/1000G.ped chr${chrom} /projects/ps-gymreklab/helia/ensembl/ensemble_out/ensemble_chr${chrom}_filtered.vcf.gz > /projects/ps-gymreklab/helia/ensembl/1000Genomes-TR-Analysis/mend/result/mend_gt_chr${chrom}.tab
 
 
 #./MI_gt_analysis.py /projects/ps-gymreklab/helia/TR_1000G/1000G.ped chr${chrom} /projects/ps-gymreklab/helia/ensembl/1000G_calls/adVNTR/chrs/all_pop_filtered_chr${chrom}.vcf.gz > /projects/ps-gymreklab/helia/ensembl/1000Genomes-TR-Analysis/mend/result/mend_gt_chr${chrom}_advntr.tab
