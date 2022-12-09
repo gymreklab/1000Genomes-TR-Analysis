dcd7cd8c3cb4eb50a1bcce89f65997db98b51d48
Helyaneh Ziaei-jam <hziaeija@tscc-login11.sdsc.edu>
2022-12-09 13:11:38 -0800

    minor updates

--- afreqs/extract.sh
+++ afreqs/extract.sh
@@ -4,23 +4,23 @@ PREFIX=$1
 DATADIR=/projects/ps-gymreklab/helia/ensembl
 #DATADIR=/projects/ps-gymreklab/helia/ensembl
 
-bcftools view -h ${DATADIR}/ensemble_out/ensemble_chr1_dropped.vcf.gz  > ${PREFIX}.vcf
+bcftools view -h ${DATADIR}/filtered_calls/chr1_MI_filtered.vcf.gz  > ${PREFIX}.vcf
 
 for i in {1..22}
 do
-    bcftools view -H -S ../metadata/samples.txt -R ${PREFIX}_loci.bed --no-update \
-	${DATADIR}/ensemble_out/ensemble_chr"$i"_dropped.vcf.gz  >> ${PREFIX}.vcf
+    bcftools view -H -R ${PREFIX}_loci.bed --no-update \
+	${DATADIR}/filtered_calls/chr"$i"_MI_filtered.vcf.gz  >> ${PREFIX}.vcf
 done
 
 bgzip -f ${PREFIX}.vcf
 tabix -f -p vcf ${PREFIX}.vcf.gz
 
-for pop in AFR AMR EUR SAS EAS
+for pop in AFR AMR EUR SAS EAS H3Africa
 do
     statSTR \
 	--vcf ${PREFIX}.vcf.gz \
 	--vcftype hipstr \
 	--afreq \
 	--out freqs/${PREFIX}-${pop} \
-	--samples ../metadata/${pop}_names.txt
+	--samples /projects/ps-gymreklab/helia/ensembl/experiments/allele_freq/names/${pop}_names.txt
 done
