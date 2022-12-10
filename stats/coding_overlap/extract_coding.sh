b3d56043216af6e76b72b80ee354165a5378e894
Helyaneh Ziaei-jam <hziaeija@tscc-login12.sdsc.edu>
2022-12-09 16:55:56 -0800

    update address

--- stats/coding_overlap/extract_coding.sh
+++ stats/coding_overlap/extract_coding.sh
@@ -12,16 +12,18 @@
 
 
 source /projects/ps-gymreklab/helia/ensembl/venv_3.9/bin/activate
-cd /projects/ps-gymreklab/helia/ensembl/1000Genomes-TR-Analysis/stats/coding_overlap
-bcftools view -h /projects/ps-gymreklab/helia/ensembl/ensemble_out/merged_chr"$chr"_sorted_ver2.vcf.gz > \
-/projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/heterozygosity/coding_calls_"$chr".vcf
+FILE_DIR=/projects/ps-gymreklab/helia/ensembl/1000Genomes-TR-Analysis/stats/coding_overlap
+OUT_DIR=/projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/heterozygosity
+cd $FILE_DIR
 
-grep -w chr"$chr" TR_intersect.txt | awk '{print $2}' > pos_chr"$chr".txt
+bcftools view -h /projects/ps-gymreklab/helia/ensembl/filtered_calls/chr"$chr"_MI_filtered.vcf.gz > \
+$OUT_DIR/coding_calls_"$chr".vcf &&
 
-bcftools view -H /projects/ps-gymreklab/helia/ensembl/ensemble_out/merged_chr"$chr"_sorted_ver2.vcf.gz | grep -F -f pos_chr"$chr".txt \
->> /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/heterozygosity/coding_calls_"$chr".vcf
+grep -w chr"$chr" $FILE_DIR/TR_intersect.txt | awk '{print $2}' > $FILE_DIR/pos_chr"$chr".txt &&
 
-bgzip -f /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/heterozygosity/coding_calls_"$chr".vcf
-tabix -p vcf -f /projects/ps-gymreklab/helia/ensembl/experiments/coding_regions/heterozygosity/coding_calls_"$chr".vcf.gz
+bcftools view -H /projects/ps-gymreklab/helia/ensembl/filtered_calls/chr"$chr"_MI_filtered.vcf.gz | grep -F -f $FILE_DIR/pos_chr"$chr".txt >> $OUT_DIR/coding_calls_"$chr".vcf
+
+bgzip -f $OUT_DIR/coding_calls_"$chr".vcf
+tabix -p vcf -f $OUT_DIR/coding_calls_"$chr".vcf.gz
 
 rm pos_chr"$chr".txt
