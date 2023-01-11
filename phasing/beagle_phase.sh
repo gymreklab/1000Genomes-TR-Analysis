0690b66df6d52b1c2cefe68d5347c86c69df7614
Helyaneh Ziaei-jam <hziaeija@tscc-login2.sdsc.edu>
2023-01-10 21:23:30 -0800

    add pedigree

--- phasing/beagle_phase.sh
+++ phasing/beagle_phase.sh
@@ -6,11 +6,12 @@ CHR=$1 # Target chromosome
 REGIONS=$2 # File address containing TR coordinates one per line
 
 # Unphased TRs
-VCF=/projects/ps-gymreklab/helia/ensembl/phasing_st/filtered/chr"$CHR"_MI_filtered.vcf.gz
+VCF=/projects/ps-gymreklab/helia/ensembl/1000Genomes-TR-Analysis/phasing/filtered_calls/chr1_polymorphic.vcf.gz
 
 #Phased SNPs
-SNPS=/projects/ps-gymreklab/helia/ensembl/biallelic/snps/CCDG_14151_B01_GRM_WGS_2020-08-05_chr"$CHR".filtered.shapeit2-duohmm-phased.vcf.gz
+SNPS=http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20220422_3202_phased_SNV_INDEL_SV/1kGP_high_coverage_Illumina.chr1.filtered.SNV_INDEL_SV_phased_panel.vcf.gz
 
+SAMPLES=/projects/ps-gymreklab/helia/ensembl/1000Genomes-TR-Analysis/phasing/samples.txt
 while read p; do
   # Extracting region and window
   chr=$(echo $p | cut -d " " -f 1)
@@ -24,11 +25,11 @@ while read p; do
   echo $ID
 
   # Get unphased STRs
-  bcftools view -S samples.txt -r $REGION $VCF --no-update --output-type z --output-file ensemble.${ID}.vcf.gz
+  bcftools view -S ${SAMPLES} -r $REGION $VCF --no-update --output-type z --output-file ensemble.${ID}.vcf.gz
   bcftools index -f ensemble.${ID}.vcf.gz
 
   # Subset samples
-  bcftools view -S samples.txt -r $WINDOW $SNPS --no-update --output-type z --output-file snps.${ID}.vcf.gz
+  bcftools view -S ${SAMPLES} -r $WINDOW $SNPS --no-update --output-type z --output-file snps.${ID}.vcf.gz
   bcftools index -f snps.${ID}.vcf.gz
 
   # Concatenate STRs+SNPs
@@ -42,23 +43,23 @@ while read p; do
     bcftools index -f -t snpstr.${ID}.fixmissing.vcf.gz
 
   # Phase with Beagle
-  java -Xmx5g -jar /projects/ps-gymreklab/helia/ensembl/phasing_st/phased/beagle.r1399.jar \
+  java -Xmx5g -jar /projects/ps-gymreklab/helia/ensembl/1000Genomes-TR-Analysis/phasing/beagle.r1399.jar \
     gt=snpstr.${ID}.fixmissing.vcf.gz \
     out="${ID}".phased usephase=true \
-    nthreads=4 &&
+    nthreads=4 ped=pedigree.fam &&
 
   tabix -p vcf -f "${ID}".phased.vcf.gz
 
   # Make sure phase is consistent with original SNP file
-  ./fix_switch_error.py \
+  /projects/ps-gymreklab/helia/ensembl/1000Genomes-TR-Analysis/phasing/fix_switch_error.py \
     --phased-vcf "${ID}".phased.vcf.gz \
     --ref-vcf snps.${ID}.vcf.gz \
     --switch-threshold 0.5 --min-maf 0.1 --check-snps 100 \
-    --new-vcf beagle_out/"$CHR"/${ID}_phased_fixed.vcf.gz &&
+    --new-vcf ${ID}_phased_fixed.vcf.gz &&
 
-  tabix -p vcf beagle_out/"$CHR"/${ID}_phased_fixed.vcf.gz
+  tabix -p vcf ${ID}_phased_fixed.vcf.gz
 
   #remove unnecessary files
-  rm ensemble.${ID}.vcf.gz* snps.${ID}.vcf.gz* snpstr.${ID}.vcf.gz* snpstr.${ID}.fixmissing.vcf.gz* "${ID}".phased.vcf.gz* "${ID}".phased.log
+  #rm ensemble.${ID}.vcf.gz* snps.${ID}.vcf.gz* snpstr.${ID}.vcf.gz* snpstr.${ID}.fixmissing.vcf.gz* "${ID}".phased.vcf.gz* "${ID}".phased.log
 
 done <$REGIONS
