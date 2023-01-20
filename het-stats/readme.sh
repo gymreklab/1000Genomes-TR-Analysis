0773f83417a7069c2d5762548df21397f8a08bce
gymreklab <melissa.gymrek@gmail.com>
2023-01-19 22:03:12 -0800

    updating figure 4 notebook

--- het-stats/readme.sh
+++ het-stats/readme.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Set up sample lists
-KGFILE=/projects/ps-gymreklab/helia/TR_1000G/1000G.ped
+KGFILE=/gymreklab-tscc/helia/TR_1000G/1000G.ped
 cat ${KGFILE} | cut -d' ' -f 2 | grep -v Sample > all_samples.txt
 for pop in EUR EAS SAS AMR AFR
 do
     cat ${KGFILE} | grep -w ${pop} | cut -d' ' -f 2 > ${pop}_samples.txt
 done
-cat /projects/ps-gymreklab/helia/H3Africa/names/H3A_Baylor_sample_country.txt | \
+cat /gymreklab-tscc/helia/H3Africa/names/H3A_Baylor_sample_country.txt | \
     cut -f 1 > h3africa_samples.txt
 
 # Compute het, n_alleles for all and for each superpopulation
-DATADIR=/projects/ps-gymreklab/helia/ensembl/ensemble_out
-for chrom in $(seq 1 14)
+DATADIR=/gymreklab-tscc/helia/ensembl/ensemble_out
+for chrom in $(seq 1 22)
 do
     echo $chrom
     statSTR \
 	--vcf ${DATADIR}/ensemble_chr${chrom}_filtered.vcf.gz \
 	--vcftype hipstr \
-	--het --nalleles --nalleles-thresh 0.01 \
+	--het --nalleles --nalleles-thresh 0.01 --use-length \
 	--samples all_samples.txt,EUR_samples.txt,EAS_samples.txt,SAS_samples.txt,AMR_samples.txt,AFR_samples.txt,h3africa_samples.txt \
 	--sample-prefixes ALL,EUR,EAS,SAS,AMR,AFR,H3A \
 	--out chr${chrom}.stats
